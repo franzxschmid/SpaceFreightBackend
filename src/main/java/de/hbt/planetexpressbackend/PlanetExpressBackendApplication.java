a1ca54b2a02a6f324a5d069110f7e52e82c31644
fxs <fxs@hbt.de>
2020-10-12 09:59:41 +0200

    Initial Commit

---
+++ src/main/java/de/hbt/planetexpressbackend/PlanetExpressBackendApplication.java
@@ -0,0 +1,13 @@
+package de.hbt.planetexpressbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetExpressBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetExpressBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/hbt/planetexpressbackend/PlanetExpressBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hbt.planetexpressbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetExpressBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
