27f46202b073d807fe6c8c3003216cb1b9dcbbb1
fxs <fxs@hbt.de>
2020-11-12 16:31:25 +0100

    Added CrossOrigin for Frontend

--- src/main/java/de/hbt/planetexpressbackend/control/PartRepository.java
+++ src/main/java/de/hbt/planetexpressbackend/control/PartRepository.java
@@ -3,8 +3,10 @@ package de.hbt.planetexpressbackend.control;
 
 import de.hbt.planetexpressbackend.entity.Part;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 // Interface mit Methoden zum: Lesen, Löschen und Erstellen gegen einen Backend-Speicher
+@Repository
 public interface PartRepository extends JpaRepository<Part, Long> {
 
 }
